a7ddeefff541fcf3ccfaed35e29dadf317589a74
sloDevz <onec555@gmail.com>
2023-06-29 16:48:43 +0900

    feat: #2 프로젝트 파일 생성

---
+++ GameScope/GameScope/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameScope
+//
+//  Created by DONGWOOK SEO on 2023/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
