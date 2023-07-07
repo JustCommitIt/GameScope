e20caa31b69b1441ada0a2dee7b3e86b321c0866
ohdair <ohdiar@gmail.com>
2023-07-07 11:31:59 +0900

    feat: #10 DetailViewController 추가
    - SectionKind 구역별로 나눌 수 있게 열거형으로 구분
    - DiffableDataSource의 사용을 위한 GameDetail Hashable 채택

---
+++ GameScope/GameScope/DetailViewController.swift
@@ -0,0 +1,94 @@
+//
+//  DetailViewController.swift
+//  GameScope
+//
+//  Created by 박재우 on 2023/07/01.
+//
+
+import UIKit
+import SnapKit
+
+class DetailViewController: UIViewController, UICollectionViewDelegate {
+    static let headerElementKind = "header-element-kind"
+
+    enum SectionKind: String, CaseIterable {
+        case thumbnail
+        case about = "About"
+        case information = "Information"
+        case screenshots = "ScreenShots"
+    }
+
+    var dataSource: UICollectionViewDiffableDataSource<SectionKind, GameDetail>! = nil
+    var detailCollectionView: UICollectionView! = nil
+
+    var detail: GameDetail?
+    var thumbnailImage: UIImage?
+
+    convenience init(gameDetail: GameDetail, withThumbnail thumbnail: UIImage) {
+        self.init()
+        detail = gameDetail
+        thumbnailImage = thumbnail
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = detail?.title
+        configureCollectionView()
+        configureDataSource()
+    }
+}
+
+extension DetailViewController {
+    func configureCollectionView() {
+        let collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: generateLayout())
+        view.addSubview(collectionView)
+        collectionView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        collectionView.backgroundColor = .systemBackground
+        collectionView.delegate = self
+        collectionView.register(HeaderView.self, forSupplementaryViewOfKind: DetailViewController.headerElementKind, withReuseIdentifier: HeaderView.reuseIdentifier)
+        detailCollectionView = collectionView
+    }
+
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<SectionKind, GameDetail>(collectionView: detailCollectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, item: GameDetail) -> UICollectionViewCell? in
+            let sectionType = SectionKind.allCases[indexPath.section]
+            switch sectionType {
+            case .thumbnail:
+                return nil
+            case .about:
+                return nil
+            case .information:
+                return nil
+            case .screenshots:
+                return nil
+            }
+        }
+
+        dataSource.supplementaryViewProvider = {
+            (collectionView: UICollectionView, kind: String, indexPath: IndexPath) -> UICollectionReusableView? in
+            guard let supplementaryView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: HeaderView.reuseIdentifier, for: indexPath) as? HeaderView else {
+                fatalError("Cannot create header view")
+            }
+            supplementaryView.label.text = SectionKind.allCases[indexPath.section].rawValue
+            return supplementaryView
+        }
+    }
+
+    func generateLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            let sectionLayoutKind = SectionKind.allCases[sectionIndex]
+            switch sectionLayoutKind {
+            case .thumbnail:
+                return nil
+            case .about:
+                return nil
+            case .information:
+                return nil
+            case .screenshots:
+                return nil
+            }
+        }
+        return layout
+    }
+}
